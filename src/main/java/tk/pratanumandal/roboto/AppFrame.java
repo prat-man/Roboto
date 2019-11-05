20cd7608225dc92c639cc09bce6ffd0920684d4d
Pratanu Mandal <pratanu.mandal@gmail.com>
2019-11-05 20:15:36 +0530

    Multiple UI and functionality changes, added application icon, added tray icon and notifications
    Multiple UI and functionality changes, added application icon, added tray icon and notifications

--- src/main/java/tk/pratanumandal/roboto/AppFrame.java
+++ src/main/java/tk/pratanumandal/roboto/AppFrame.java
@@ -6,12 +6,20 @@ import java.awt.Cursor;
 import java.awt.Desktop;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Insets;
 import java.awt.Robot;
+import java.awt.SystemTray;
 import java.awt.Toolkit;
+import java.awt.TrayIcon;
+import java.awt.TrayIcon.MessageType;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Random;
@@ -20,10 +28,13 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 
@@ -34,57 +45,81 @@ public class AppFrame extends JFrame {
 	private ScheduledFuture<?> mouseFuture;
 	private ScheduledFuture<?> keyboardFuture;
 	
+	private TrayIcon trayIcon;
+	
 	public AppFrame() {
+		// call super constructor and set frame title
 		super("Roboto");
 		
+		// set frame icon
+		try {
+			String imagePath = "robot.png";
+			InputStream imgStream = this.getClass().getClassLoader().getResourceAsStream(imagePath);
+			BufferedImage image = ImageIO.read(imgStream);
+			this.setIconImage(image);
+		} catch (IOException exc) {
+			exc.printStackTrace();
+		}
 		
+		// initialize tray icon
+		initializeTray();
+		
+		// initialize container which acts as a wrapper for mousePanel and keyboardPanel
 		JPanel container = new JPanel();
 		container.setLayout(new GridLayout(2, 1));
+		Border padding = BorderFactory.createEmptyBorder(10, 10, 10, 10);
+		container.setBorder(padding);
 		
+		this.add(container, BorderLayout.CENTER);
 		
+		// initialize mouse panel
 		JPanel mousePanel = new JPanel();
-		mousePanel.setBorder(new TitledBorder("Mouse"));
+		mousePanel.setBorder(new TitledBorder("<html><span style=\"font-size: 1.1em; font-weight: bold;\">Mouse</span></html>"));
 		
 		JButton mouseStartButton = new JButton("Start");
-		mouseStartButton.setPreferredSize(new Dimension(100, 25));
+		mouseStartButton.setMargin(new Insets(3, 35, 3, 35));
+		mouseStartButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		mousePanel.add(mouseStartButton);
 		
 		JButton mouseStopButton = new JButton("Stop");
-		mouseStopButton.setPreferredSize(new Dimension(100, 25));
+		mouseStopButton.setMargin(new Insets(3, 35, 3, 35));
+		mouseStopButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		mouseStopButton.setEnabled(false);
 		mousePanel.add(mouseStopButton);
 		
 		container.add(mousePanel);
 		
-		
+		// initialize keyboard panel
 		JPanel keyboardPanel = new JPanel();
-		keyboardPanel.setBorder(new TitledBorder("Keyboard"));
+		keyboardPanel.setBorder(new TitledBorder("<html><span style=\"font-size: 1.1em; font-weight: bold;\">Keyboard</span></html>"));
 		
 		JButton keyboardStartButton = new JButton("Start");
-		keyboardStartButton.setPreferredSize(new Dimension(100, 25));
+		keyboardStartButton.setMargin(new Insets(3, 35, 3, 35));
+		keyboardStartButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		keyboardPanel.add(keyboardStartButton);
 		
 		JButton keyboardStopButton = new JButton("Stop");
-		keyboardStopButton.setPreferredSize(new Dimension(100, 25));
+		keyboardStopButton.setMargin(new Insets(3, 35, 3, 35));
+		keyboardStopButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		keyboardStopButton.setEnabled(false);
 		keyboardPanel.add(keyboardStopButton);
 		
 		container.add(keyboardPanel);
 		
 		
-		this.add(container, BorderLayout.CENTER);
-		
-		
+		// initialize information panel
 		JPanel infoPanel = new JPanel();
-		infoPanel.setBorder(new EmptyBorder(0, 0, 3, 0));
-		
-		JLabel infoLabel = new JLabel("<html><span style=\"color: blue;\">Pratanu Mandal & Jerry Jose</span></html>");
-		infoLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		infoPanel.add(infoLabel);
+		infoPanel.setBorder(new EmptyBorder(0, 0, 15, 0));
 		
 		this.add(infoPanel, BorderLayout.SOUTH);
 		
+		// initialize information label
+		JLabel infoLabel = new JLabel("<html>Developed by <span style=\"color: blue;\">Pratanu Mandal</span></html>");
+		infoLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		infoPanel.add(infoLabel);
 		
+		
+		// start action for mouse
 		mouseStartButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -107,6 +142,7 @@ public class AppFrame extends JFrame {
 					}, 0, 5, TimeUnit.SECONDS);
 					
 					System.out.println("Mouse Scheduler Started");
+					displayTray("Roboto", "Mouse Scheduler Started");
 					
 					mouseStartButton.setEnabled(false);
 					mouseStopButton.setEnabled(true);
@@ -116,19 +152,21 @@ public class AppFrame extends JFrame {
 			}
 		});
 		
+		// stop action for mouse
 		mouseStopButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				mouseFuture.cancel(true);
 				
 				System.out.println("Mouse Scheduler Stopped");
+				displayTray("Roboto", "Mouse Scheduler Stopped");
 				
 				mouseStartButton.setEnabled(true);
 				mouseStopButton.setEnabled(false);
 			}
 		});
 		
-		
+		// start action for keyboard
 		keyboardStartButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -163,6 +201,7 @@ public class AppFrame extends JFrame {
 					}, 0, 1, TimeUnit.SECONDS);
 					
 					System.out.println("Keyboard Scheduler Started");
+					displayTray("Roboto", "Keyboard Scheduler Started");
 					
 					keyboardStartButton.setEnabled(false);
 					keyboardStopButton.setEnabled(true);
@@ -172,19 +211,21 @@ public class AppFrame extends JFrame {
 			}
 		});
 		
+		// stop action for keyboard
 		keyboardStopButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				keyboardFuture.cancel(true);
 				
 				System.out.println("Keyboard Scheduler Stopped");
+				displayTray("Roboto", "Keyboard Scheduler Stopped");
 				
 				keyboardStartButton.setEnabled(true);
 				keyboardStopButton.setEnabled(false);
 			}
 		});
 		
-		
+		// information label action
 		infoLabel.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -220,5 +261,52 @@ public class AppFrame extends JFrame {
 		}
 		return false;
 	}
+	
+	public void initializeTray() {
+		if (trayIcon == null) {
+	        // obtain only one instance of the SystemTray object
+	        SystemTray tray = SystemTray.getSystemTray();
+	        
+	        // obtain image
+			Image image = Toolkit.getDefaultToolkit().createImage(getClass().getClassLoader().getResource("robot.png"));
+	
+			// create tray icon
+	        trayIcon = new TrayIcon(image, "Roboto");
+	        
+	        // let the system resize the image if needed
+	        trayIcon.setImageAutoSize(true);
+	        
+	        // set tooltip text for the tray icon
+	        trayIcon.setToolTip("Roboto");
+	        
+	        trayIcon.addMouseListener(new MouseAdapter() {
+	        	@Override
+	        	public void mouseClicked(MouseEvent e) {
+	        		if (AppFrame.this.getState() == JFrame.ICONIFIED) {
+		        		AppFrame.this.setState(JFrame.NORMAL);
+		        		AppFrame.this.toFront();
+		        		super.mouseClicked(e);
+	        		}
+	        		else {
+	        			AppFrame.this.setState(JFrame.ICONIFIED);
+		        		super.mouseClicked(e);
+	        		}
+	        	}
+			});
+	        
+	        // add trayicon to system tray
+	        try {
+				tray.add(trayIcon);
+			} catch (AWTException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void displayTray(String title, String message) {
+		initializeTray();
+        // display message
+        trayIcon.displayMessage(title, message, MessageType.NONE);
+    }
 	
 }
